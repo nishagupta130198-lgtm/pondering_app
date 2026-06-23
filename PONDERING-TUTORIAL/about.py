db66beb37967f1b07c80a5964a9ff70680f5f3aa
Nisha_Jais <nishagupta130198@gmail.com>
2026-06-23 16:16:40 +0530

    Add files via upload

---
+++ PONDERING-TUTORIAL/about.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def app():
+    st.subheader('Pondering is a website created for users to')
+    st.markdown('Created by: Nisha Gupta')
+    st.markdown('Contact via mail: [nishagupta130198@gmail.com]')
+    
